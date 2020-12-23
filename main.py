18aef80cf1dd0fbf397bf9a24853f2c0634c5475
kmorguen <zeuspadawans@gmail.com>
2020-12-23 14:35:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+import os
+import requests
+import json
+
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+  
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$bitch'):
+        await message.channel.send('Pas de gros mot svp')
+
+    if message.content.startswith('$inspire'):
+      quote = get_quote()
+      await message.channel.send(quote)
+
+token = os.environ.get("DISCORD_BOT_SECRET") 
+client.run(token)
